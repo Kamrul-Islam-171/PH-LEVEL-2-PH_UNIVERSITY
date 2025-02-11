fe95935318718236713818259fdcafa91d5c87e8
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2025-02-11 16:36:59 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin:['http://localhost:5173'], credentials:true}));
+app.use(cors({origin:['http://localhost:5173', 'http://localhost:5174'], credentials:true}));
 app.use(cookieParser())
 
 //application routes
